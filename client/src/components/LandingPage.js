03e9f752177c13aa3b0c0f2a6ac7275e37739cb4
viivi-uhari <viivi.uhari@gmail.com>
2023-10-07 13:44:48 +0300

    Fix landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -6,15 +6,13 @@ const introduction = "Sed do eiusmod tempor ut labore et dolore magna aliqua. Ut
 const LandingPage = () => {
   return (
     <div className="App">
-      <div className="landing">
-        <div className="landing-background">
-          <svg className="landing-ellipse">
-            <ellipse cx="50%" cy="80vh" rx="130%" ry="500"/>
-          </svg>
-          <div className="landing-content">
-            <h1>Welcome</h1>
-            <p>{introduction}</p>
-          </div>
+      <div className="landing-background">
+        <svg className="landing-ellipse">
+          <ellipse cx="50%" cy="80vh" rx="130%" ry="500"/>
+        </svg>
+        <div className="landing-content">
+          <h1>Welcome</h1>
+          <p>{introduction}</p>
         </div>
       </div>
     </div>
