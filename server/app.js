61976c9f7e24b3b1ebc0c8908d27636cd5a88e79
Milla Porttilahti <milla.porttilahti@aalto.fi>
2023-10-09 17:45:42 +0300

    Move connecting to db to app

--- server/app.js
+++ server/app.js
@@ -1,5 +1,17 @@
+const config = require('./utils/config')
 const express = require("express")
 const app = express()
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('Error connecting to MongoDB:', error.message)
+  })
 
 app.use(express.json())
 
