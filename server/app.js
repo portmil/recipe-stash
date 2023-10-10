3775eaaa1f21b0d50edf315a51add02c1bc8001f
Milla Porttilahti <milla.porttilahti@aalto.fi>
2023-10-10 11:47:59 +0300

    Implement user registration on server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 const config = require('./utils/config')
 const express = require("express")
 const app = express()
+const recipesRouter = require('./controllers/recipes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -18,5 +22,16 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
+
+app.use('/api/recipes',
+  middleware.tokenExtractor,
+  middleware.userExtractor,
+  recipesRouter
+)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
