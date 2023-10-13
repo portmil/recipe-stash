5b3be4d589512f2eb67335a066375d01e5bba3f4
viivi-uhari <viivi.uhari@gmail.com>
2023-10-13 10:00:09 +0300

    Create navigation

---
+++ client/src/components/AddRecipePage.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+const AddRecipePage = () => {
+  return (
+    <div className="App">
+      <h1>Add Recipe</h1>
+    </div>
+  );
+}
+
+export default AddRecipePage;
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import '../styles/App.css';
+
+const HomePage = () => {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+    </div>
+  );
+}
+
+export default HomePage;
