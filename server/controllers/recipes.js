46550809d33ba81640edc9e514cd8cd13936f0a1
Milla Porttilahti <milla.porttilahti@aalto.fi>
2023-10-14 21:18:43 +0300

    Add requests for recipe management
    Adds request handling for fetching all recipes (of a specific user), fetching a single recipe, and deleting a recipe.

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,33 +1,98 @@
 const recipesRouter = require('express').Router()
 const Recipe = require('../models/recipe')
+const Category = require('../models/category')
 
 
-recipesRouter.post('/', async (request, response) => {
+recipesRouter.get('/', async (request, response) => {
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: 'invalid user: no token provided' })
+  }
+
+  const recipes = await Recipe
+    .find({userId : user.id}) // users can view only their own recipes
+  response.json(recipes)
+})
+
+
+recipesRouter.get('/:id', async (request, response, next) => {
+  const user = request.user
+  try {
+    const recipe = await Recipe.findById(request.params.id)
+    if (!recipe) {
+      return response.status(404).json({ error: 'recipe not found' })
+    }
+    if (!user || recipe.userId.toString() !== user.id.toString()) {
+      return response.status(401).json({ error: 'invalid user' })
+    }
+
+    // valid token and valid user
+    response.json(recipe)
+
+  } catch (error) {
+    next(error) // give error to error handler middleware
+  }
+})
+
+
+recipesRouter.post('/', async (request, response, next) => {
   const { name, link, description, rating, lastMakingDate } = request.body
   const user = request.user
 
   if (!user) {
     return response.status(401).json({ error: 'invalid user: no token provided' })
   }
-  
-  // Check that name isn't missing
-  if (name) {
-    const recipe = new Recipe({
-      name,
-      link,
-      description,
-      rating,
-      lastMakingDate,
-      userId: user._id
-    })
 
+  const recipe = new Recipe({
+    name,
+    link,
+    description,
+    rating: rating || 0,
+    lastMakingDate,
+    userId: user._id
+  })
+
+  // The format of the data is validated by Mongoose's validation
+  // functionality as specified in the recipe schema
+  try {
     const savedRecipe = await recipe.save()
     user.recipes = user.recipes.concat(savedRecipe._id)
     await user.save()
-
     response.status(201).json(savedRecipe)
-  } else { // Name is missing -> don't save the recipe
-    response.status(400).end()
+  } catch (error) {
+    next(error) // give error to error handler middleware
+  }
+})
+
+
+recipesRouter.delete('/:id', async (request, response, next) => {
+  const user = request.user
+  const idToDelete = request.params.id
+  try {
+    const recipe = await Recipe.findById(idToDelete)
+    if (!recipe) { // recipe has already been deleted
+      return response.status(204).end()
+    }
+    if (!user || recipe.userId.toString() !== user.id.toString()) {
+      return response.status(401).json({ error: 'invalid user' })
+    }
+
+    // valid token and valid user
+    await Recipe.findByIdAndRemove(idToDelete)
+    user.recipes = user.recipes.filter(id => id.toString() !== idToDelete)
+    await user.save()
+
+    recipe.categories.forEach(async (catId) => { // remove recipe from categories
+      const cat = await Category.findById(catId)
+      cat.unrankedRecipes = cat.unrankedRecipes.filter(id => id.toString() !== idToDelete)
+      cat.rankedRecipes = cat.rankedRecipes.filter(id => id.toString() !== idToDelete)
+      await cat.save()
+    })
+    
+    response.status(204).end()
+
+  } catch (error) {
+    next(error) // give error to error handler middleware
   }
 })
 
--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose')
 
 const recipeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
+  name: { type: String, required: [true, "is required"] },
   link: String,
   description: String,
-  rating: { type: Number, min: 1, max: 5 },
+  rating: { // User can enter integer values 1-5, and unrated recipes have 0
+    type: Number,
+    min: 0,
+    max: 5,
+    validate: {
+      validator: Number.isInteger,
+      message: "{VALUE} is not an integer value"
+    }
+  },
   lastMakingDate: { type: Date },
-  // categories: [{ type: ObjectId, ref: 'Category' }],
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 })
 
 recipeSchema.set('toJSON', {
