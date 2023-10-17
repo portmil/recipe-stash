7786ae1ed16f63db0f91b727679813da405e3bb7
Kiia Mäkirinta <kiia.makirinta@gmail.com>
2023-10-17 14:33:49 +0300

    icon lists added

---
+++ client/src/graphics/categoryIcons.js
@@ -0,0 +1,37 @@
+const defaultIcons = [
+  'all_icon.svg',
+  'appetizer_icon.svg',
+  'bread_icon.svg',
+  'breakfast_icon.svg',
+  'casserole_icon.svg',
+  'coldDrink_icon.svg',
+  'dairy_icon.svg',
+  'dessert_icon.svg',
+  'dinner_icon.svg',
+  'fish_icon.svg',
+  'fruit_icon.svg',
+  'hotDrink_icon.svg',
+  'lunch_icon.svg',
+  'meat_icon.svg',
+  'pasta_icon.svg',
+  'poultry_icon.svg',
+  'salad_icon.svg',
+  'sauce_icon.svg',
+  'soup_icon.svg',
+  'vegan_icon.svg',
+  'vegetarian_icon.svg',
+];
+
+const extraIcons = [
+  'noodles_icon.svg',
+  'cheese_icon.svg',
+  'watermelon_icon.svg',
+  'egg_icon.svg',
+];
+
+const categoryIcons = {
+  defaultIcons,
+  extraIcons,
+};
+
+export default categoryIcons;
