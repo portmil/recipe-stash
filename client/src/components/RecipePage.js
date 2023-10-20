ee82fa28888b34f3a1c7c04b0d929f9e35d12d7e
Kiia Mäkirinta <kiia.makirinta@gmail.com>
2023-10-20 09:50:15 +0300

    recipe page started

---
+++ client/src/components/RecipePage.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+import React from 'react';
+
+const RecipePage = () => {
+  return (
+    <div className='App'>
+      <h1>Recipe</h1>
+    </div>
+  );
+};
+
+export default RecipePage;
