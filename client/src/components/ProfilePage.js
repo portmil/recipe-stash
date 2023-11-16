e257560a0cca213de5aef7e78662a5b2dadd66cf
viivi-uhari <viivi.uhari@gmail.com>
2023-11-16 13:36:31 +0200

    Create profile page

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,10 +1,37 @@
 import '../styles/App.css';
+import '../styles/ProfilePage.css';
 import { useNavigate } from 'react-router-dom';
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import LogoutIcon from '../graphics/logout_icon.svg';
+import { ReactComponent as UserIcon } from '../graphics/user_icon.svg';
+import { ReactComponent as RecipesAddedIcon } from '../graphics/recipes_added_icon.svg';
+import { ReactComponent as RecipesCookedIcon } from '../graphics/recipes_cooked_icon.svg';
+import recipeService from '../services/recipes.js';
 
 const ProfilePage = () => {
 
   const navigate = useNavigate();
+
+  const user = JSON.parse(window.localStorage.getItem('user'));
+
+  const [recipes, setRecipes] = useState([]);
+  const [cookedRecipes, setCookedRecipes] = useState([]);
+
+  useEffect(() => {
+    /* The callback function passed to useEffect() cannot be async, so 
+       an immediately invoked function expression has to be used instead */
+    (async () => {
+      try {
+        const recipes = await recipeService.getAll();
+        setRecipes(recipes);
+        setCookedRecipes(recipes.filter(recipe => {
+          return new Date(recipe.lastMakingDate).toLocaleDateString() !== new Date(0).toLocaleDateString();
+        }));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
 
   const handleLogout = () => {
     window.localStorage.removeItem('user');
@@ -12,9 +39,36 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className='App'>
-      <h1>Profile</h1>
-      <button className='primary-btn' onClick={handleLogout}>LOGOUT</button>
+    <div className='profile-page-container'>
+      <button className='logout-button' onClick={handleLogout}>
+        <img 
+          src={LogoutIcon} 
+          alt={'Logout icon'}
+        />
+      </button>
+      <div className='user-info'>
+        <UserIcon/>
+        <div className='user-text-container'>
+          <h1 className='user-text'>{user.name}</h1>
+          <p className='user-text'>{user.email}</p>
+        </div>
+      </div>
+      <div className='user-recipe-info'>
+        <div className='summary-box'>
+          <RecipesAddedIcon className='user-recipe-icon'/>
+          <p className='user-recipe-text'>{recipes.length}</p>
+          <p className='user-recipe-text'>
+            Recipes<br/>added
+          </p>
+        </div>
+        <div className='summary-box'>
+          <RecipesCookedIcon className='user-recipe-icon'/>
+          <p className='user-recipe-text'>{cookedRecipes.length}</p>
+          <p className='user-recipe-text'>
+            Recipes<br/>cooked
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
