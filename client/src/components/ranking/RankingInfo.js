2ca9393c643af0b474097ba77b2f5d8913337c45
viivi-uhari <viivi.uhari@gmail.com>
2023-11-24 14:26:00 +0200

    Refine ranking info style

--- client/src/components/ranking/RankingInfo.js
+++ client/src/components/ranking/RankingInfo.js
@@ -11,8 +11,12 @@ const RankingInfo = () => {
     modal nested>
       { close => (
         <div className='content'>
+          <button className='close-popup-button' type='button' onClick={() => {
+            close();
+          }}>X</button>
           <div className='popup-container ranking'>
             <p className='ranking-info-text'>Begin ranking recipes by switching on edit mode.</p>
+            <p className='ranking-info-text'>You can rank recipes within the selected category.</p>
             <p className='ranking-info-text'>Then drag and drop recipes from the unranked column to the ranked column.</p>
             <button type='button' className='primary-btn' onClick={close}>
               BACK
